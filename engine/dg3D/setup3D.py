7ab2a5ca5b3e390326fd6589f0a304c692b5bd52
André Fabiano <andrefsr1902@gmail.com>
2026-07-30 12:23:53 -0300

    dg3D - Adicionado StartUp3D e o dtscale3D

--- engine/dg3D/setup3D.py
+++ engine/dg3D/setup3D.py
@@ -1,8 +1,8 @@
 from types import SimpleNamespace
 import numpy as np
 from Nodes3D import Nodes3D, xyztorst
-from operators3D import Vandermonde3D, Dmatrices3D, calcular_fmask3D, Lift3D
-from grid3D import GeometricFactors3D
+from operators3D import Vandermonde3D, Dmatrices3D, calcular_fmask3D, Lift3D, GradVandermonde3D
+from grid3D import GeometricFactors3D, Normals3D, tiConnect3D, BuildMaps3D
 
 def StartUp3D(N,EToV,VX,VY,VZ):
 
@@ -10,6 +10,7 @@ def StartUp3D(N,EToV,VX,VY,VZ):
     Nfp = int((N+1)*(N+2)/2)
     Nfaces = 4
     NODETOOL = 1e-7
+    K = EToV.shape[0]
 
     x, y, z = Nodes3D(N)
     r, s, t = xyztorst(x,y,z)
@@ -42,19 +43,43 @@ def StartUp3D(N,EToV,VX,VY,VZ):
 
     rx, ry, rz, sx, sy, sz, tx, ty, tz, J = GeometricFactors3D(x,y,z,Dr,Ds,Dt)
 
-    nx, ny, nz, sJ = 
+    nx, ny, nz, sJ = Normals3D(rx,ry,rz,sx,sy,sz,tx,ty,tz,J,Fmask,N,K)
     Fscale = sJ/J[fmask_flat,:]
+
+    EToE, EToF = tiConnect3D(EToV)
+
+    vmapM, vmapP, vmapB, mapB = BuildMaps3D(K,Np,Nfp,Nfaces,Fmask,EToE,EToF,x,y,z,NODETOOL)
+
+    Vr, Vs, Vt = GradVandermonde3D(N,r,s,t)
+
+    Drw = (V @ Vr.T) @ np.linalg.inv(V @ V.T)
+    Dsw = (V @ Vs.T) @ np.linalg.inv(V @ V.T)
+    Dtw = (V @ Vt.T) @ np.linalg.inv(V @ V.T)
+
 
     # Empacota TODAS as variáveis da malha em um único objeto organizado
     malha = SimpleNamespace(
-        N=N, Np=Np, Nfp=Nfp, Nfaces=Nfaces, K=EToV.shape[0],
-        r=r, s=s,t=t, x=x, y=y,z=z,
+        N=N, Np=Np, Nfp=Nfp, Nfaces=Nfaces, K=K,
+        r=r, s=s, t=t, x=x, y=y, z=z,
         V=V, invV=invV, MassMatrix=MassMatrix, 
-        Dr=Dr, Ds=Ds,Dt=Dt, Drw=Drw, Dsw=Dsw, LIFT=LIFT,
-        rx=rx, sx=sx, ry=ry, sy=sy, J=J,
-        nx=nx, ny=ny, sJ=sJ, Fscale=Fscale,
-        EToE=EToE, EToF=EToF, Fmask=Fmask, Fx=Fx, Fy=Fy,
-        mapM=mapM, mapP=mapP, vmapM=vmapM, vmapP=vmapP, vmapB=vmapB, mapB=mapB
+        Dr=Dr, Ds=Ds, Dt=Dt, Drw=Drw, Dsw=Dsw, Dtw=Dtw, LIFT=LIFT,
+        rx=rx, sx=sx, tx=tx, ry=ry, sy=sy, ty=ty, rz=rz, sz=sz, tz=tz, J=J,
+        nx=nx, ny=ny, nz=nz, sJ=sJ, Fscale=Fscale,
+        EToE=EToE, EToF=EToF, Fmask=Fmask, Fx=Fx, Fy=Fy, Fz=Fz, 
+        vmapM=vmapM, vmapP=vmapP, vmapB=vmapB, mapB=mapB #,mapM=mapM, mapP=mapP
     )
     
     return malha
+
+def dtscale3D(J,sJ):
+    '''Calcula o raio do círculo inscrito de cada triângulo da malha 
+    para ser usado na condição de estabilidade CFL (passo de tempo).'''
+
+    J_elem = J[0, :]
+    sJ_max_elem = np.max(sJ, axis=0)  
+
+    dtscale_elem = J_elem / sJ_max_elem  
+
+    dtscale_global = np.min(dtscale_elem)
+    
+    return dtscale_global
